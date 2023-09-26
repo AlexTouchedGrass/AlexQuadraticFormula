151791ca1474534ef1885762a014acf18be18cd6
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-09-26 09:30:53 -0400

    This was hard.

--- src/AlexQuadraticFormula.java
+++ src/AlexQuadraticFormula.java
@@ -3,14 +3,26 @@ import java.util.Scanner;
 public class AlexQuadraticFormula {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int a;
-        int b;
-        int c;
-        double answerQuad;
-
+        double a, b, c = 0;
+        double answerQuad1 = 0;
+        double answerQuad2 = 0;
 
 
         System.out.println("Please enter the values for a,b, and c in standard form.");
+        a = scan.nextInt();
+        b = scan.nextInt();
+        c = scan.nextInt();
 
+        if (Math.pow(b, 2) - 4 * a * c < 0) {
+            System.out.println("Sorry your answer is imaginary.");
+
+
+        }
+        else {
+            answerQuad1 = -1*b + Math.sqrt(Math.pow(b, 2) - 4 * a * c);
+            answerQuad2 = -1*b - Math.sqrt(Math.pow(b, 2) - 4 * a * c);
+
+            System.out.println("The answers to the quadratic formula using the a,b, and c inputs previous is " + answerQuad1 + " and " + answerQuad2 + ".");
+        }
     }
 }
